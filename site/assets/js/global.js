9218c857a75e540e827c8eb6b229c8e43a1528f2
hstrada <h.strada@hotmail.com>
2020-07-05 16:16:27 -0300

    feat: add mutationobserver

--- site/assets/js/global.js
+++ site/assets/js/global.js
@@ -1,2 +1,17 @@
 // generator
 let divGenerator = document.getElementById("generator--border");
+
+let divCode = document.getElementById("generator--code");
+
+var observer = new MutationObserver(function (mutations) {
+  mutations.forEach(function (mutationRecord) {
+    divCode.innerText = `border-width: ${divGenerator.style.borderWidth}
+                        border-radius: ${divGenerator.style.borderRadius}
+                        border-color: ${divGenerator.style.borderColor}
+                        border-style: ${divGenerator.style.borderStyle}
+                        `;
+  });
+});
+
+var target = document.getElementById("generator--border");
+observer.observe(target, { attributes: true, attributeFilter: ["style"] });
