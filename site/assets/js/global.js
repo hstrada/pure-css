804cc4742c20c648d3e493a83c983d8108e415f3
Helena Strada <h.strada@hotmail.com>
2020-08-19 09:53:14 -0300

    feat: adjust output for border generator

--- site/assets/js/global.js
+++ site/assets/js/global.js
@@ -5,10 +5,10 @@ let divCode = document.getElementById("generator--code");
 
 var observer = new MutationObserver(function (mutations) {
   mutations.forEach(function (mutationRecord) {
-    divCode.innerText = `border-width: ${divGenerator.style.borderWidth}
-                        border-radius: ${divGenerator.style.borderRadius}
-                        border-color: ${divGenerator.style.borderColor}
-                        border-style: ${divGenerator.style.borderStyle}
+    divCode.innerText = `border-width: ${divGenerator.style.borderWidth};
+                        border-radius: ${divGenerator.style.borderRadius};
+                        border-color: ${divGenerator.style.borderColor};
+                        border-style: ${divGenerator.style.borderStyle};
                         `;
   });
 });
